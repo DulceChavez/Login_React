1518d5f31bdce31dac6eb57369c3a818ceb0fe05
DulceChavez <dulce.chavez@ghimprove.com>
2023-03-14 22:01:55 -0600

    Validacion de usuario

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
+import React, { useState } from "react";
+import swal from "sweetalert";
 function App() {
-
+  const [Usuario, ValUsua] = useState(false);
+  const [Contraseña, ValContra] = useState(false);
+  const [Error, enviarError] = useState("");
+  //const [Tipo, TipoContraseña] = useState();
+  const usuarios = (e)=>{
+      let usuario =  e.target.value 
+    if(usuario === 'Dulce'){
+      ValUsua (true)
+  }
+  else{
+    ValUsua(false)
+  }
+}  
+const contraseñas = (e) =>{
+  let contraseña = e.target.value
+  if(contraseña === "Dulce050603" ){
+    ValContra(true)
+  }
+  else{
+    ValContra(false)
+  }
+}  
+  const enviar =()=>{
+    console.log("Enviar")
+    if (Usuario === true && Contraseña === true ){
+      swal({
+        title: "Bienvenido",
+        icon: "success"
+      });
+    } 
+    else {
+      enviarError("Usuario o Contraseña Incorrectos")
+    }
+  } 
+  /*const tipo = () =>{
+  }*/
 return (
 
   <div className="App">
     <h1>Iniciar Sesión</h1>
     <div id="cuadro" >
-      <form >
         <div >
-        <span class="material-symbols-outlined">
+        <span className ="material-symbols-outlined">
           account_circle
         </span>
-        <input placeholder="Usuario"/>
+        <input placeholder="Usuario" onChange={(e) => usuarios(e)}/>
+        
         </div>
         <div>
-        <span class="material-symbols-outlined">
+        <span className="material-symbols-outlined">
           lock
         </span>
-          <input type="password" placeholder="Contraseña"/>
-          <button type="onclick">Ver</button>
+          <input type="password " placeholder="Contraseña" onChange={(e) => contraseñas(e)}/> 
+          <button type="onclick" >Ver</button>
         </div>
-        <button type="submit" >Entrar</button>
-      </form>
+        {
+          (Error.length > 1) && (
+            <p>{Error}</p>
+          )
+        }
+        <button type="onclick" onClick={() => enviar()}>Entrar</button> 
     </div>
-    
-
-
-
   </div> 
   );
 }
---
+++ src/Prueba.js
@@ -0,0 +1,26 @@
+
+function Prueba() {
+  
+return (
+
+  <div className="App">
+    <h1>Iniciar Sesión</h1>
+    <div id="cuadro" >
+        <div >
+        <span className ="material-symbols-outlined">
+          account_circle
+        </span>
+        <input placeholder="Usuario" />
+        
+        </div>
+        <div>
+        <span className="material-symbols-outlined">
+          lock
+        </span>
+          <input  placeholder="Contraseña" />
+        </div>
+    </div>
+  </div> 
+  );
+}
+export default Prueba;
